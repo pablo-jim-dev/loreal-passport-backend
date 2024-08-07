5a27819ccd835d9ad18884aa7f631a2ad2005aa9
Pablo Jiménez <pablojimenez@MacBook-Pro-de-Pablo.local>
2024-08-06 20:21:43 -0600

    (App): Http

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import app from './app.js';
 import * as dotenv from 'dotenv'
 import { connectDB } from './db.js';
-import fs from 'fs';
-import https from 'https';
+// import fs from 'fs';
+// import https from 'https';
 
-const httpsOptions = {
-    key: fs.readFileSync('./certs/server.key'),
-    cert: fs.readFileSync('./certs/server.crt')
-}
+// const httpsOptions = {
+//     key: fs.readFileSync('./certs/server.key'),
+//     cert: fs.readFileSync('./certs/server.crt')
+// }
 
 dotenv.config();
 connectDB();
 
-// app.listen(3000, () => {
-//     console.log('Server is listening on port 3000');
-// });
-
-https.createServer(httpsOptions, app).listen(3000, () => {
+app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
+
+// https.createServer(httpsOptions, app).listen(3000, () => {
+//     console.log('Server is listening on port 3000');
+// });
